2d655e850c2b24e45a04484783e8fccc9ad84639
Samba DIOP <bathiadiop97@gmail.com>
2023-05-25 00:02:32 +0000

    First Commit

---
+++ 00.py
@@ -0,0 +1,14 @@
+print('Hollo Word!')
+
+#Demande nom et prenom
+print("Quelle est votre prénom")
+Prenom=input()
+print('Quelle est votre nom')
+Nom=input()
+print('Le nombre de caractére de votre prenom est '+ str(len(Prenom)))
+print('Le nombre de caractére de votre nom est '+ str(len(Nom)))
+# Demande l'âge
+print('How old are you')
+Age=input()
+print('Vous avez'+' '+ Age+'ans')
+print('Vous serez ' + str(int(Age) + 1) + ' dans un an.')
