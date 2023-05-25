81c83c2bfebf14ef34a9221e1f07584cdaf4ed06
Samba DIOP <bathiadiop97@gmail.com>
2023-05-25 21:38:54 +0000

    Fourth Commit

---
+++ 02.py
@@ -0,0 +1,24 @@
+# Bouble while
+# Différence entre un if et while
+# Condition if
+# spam = 0
+# if spam < 5:
+#     print('Hello world')
+#     spam = spam + 1
+
+# Condition while ou boucle while
+
+# First example
+spam = 0
+while spam < 5 :
+    print('Hello Word')
+    spam = spam + 1
+
+
+# second example
+name = ''
+while name != 'your name':
+    print('Please type your name')
+    name = input()
+print('Thanks you')
+
