ea6bc077b098f131fe44015f44af368df0e9e675
Samba DIOP <bathiadiop97@gmail.com>
2023-05-25 21:31:31 +0000

    Third Commit

---
+++ 01.py
@@ -0,0 +1,13 @@
+
+print('Quelle est votre nom')
+name=input()
+if name == 'Samba':
+    print('Hello '+ name)
+    print('Your password please: ')
+    password=input()
+    if password == 'P@ssword123':
+        print('*******************************Bienvenue à votre interface d\'accueil************************************')
+        print('Salut '+ name)
+else:
+    print('Vous n\'êtes Samba. Veuillez vous reconnecter')
+    
