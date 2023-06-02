f4af91f73e736fb6200ac953f2c84217dd30dde6
Samba DIOP <bathiadiop97@gmail.com>
2023-06-02 17:08:09 +0000

    Challenge 01

--- ChallengePython/Capital_indexes.py
+++ ChallengePython/Capital_indexes.py
@@ -1,7 +1,6 @@
 # Write a function named capital_indexes. 
 # The function takes a single parameter, which is a string. 
-# Your function should return a list of all the indexes in 
-# the string that have capital letters.
+# Your function should return a list of all the indexes in  the string that have capital letters.
 # For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
 
 
@@ -13,4 +12,5 @@ def capital_indexes(a):
             list.append(x)
     return list 
 print(capital_indexes("HeLlO"))
+
 
