5f64e8a851d41e713c0352cd2a5788ec5c78264f
Samba DIOP <bathiadiop97@gmail.com>
2023-06-02 01:00:25 +0000

    First commit Challenge Lab

--- ChallengesPython/Capital_indexes.py
+++ ChallengesPython/Capital_indexes.py
@@ -3,7 +3,6 @@
 # Your function should return a list of all the indexes in the string that have capital letters.
 # For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4].
 
-
 def capital_indexes(a):
     long = len(a)
     list = []
